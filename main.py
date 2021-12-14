90a5d75ccc24d024e3112040fa5b8e9c04960257
Jalilov <javohirjalilov777@gmail.com>
2021-12-15 00:18:21 +0500

    send quiz

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from telegram import Update, ReplyKeyboardMarkup, ReplyMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup, Document, PhotoSize
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters, PollAnswerHandler, PollHandler
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters, PollAnswerHandler, PollHandler, CallbackQueryHandler
 import db
 import os
 def start(update: Update, context: CallbackContext) -> None:
@@ -15,10 +15,35 @@ def document(update,context):
 
 def quiz(update,context):
     all_quiz = db.read_quiz_data()
+    # Create a button from the list of quizzes
     buttons = [[InlineKeyboardButton(button,callback_data=button)] for button in all_quiz]
     reply_markup = InlineKeyboardMarkup(buttons)
     update.message.reply_text(text='Choose the one you need from the Quiz below',reply_markup=reply_markup)
 
+def choose_quiz(update,context):
+    bot = context.bot
+    query = update.callback_query
+    callback_data = query.data
+    questions = db.get_quiz(callback_data)
+    chat_id = update.callback_query.message.chat.id
+    for i,quiz in enumerate(questions):
+        question = quiz['question']
+        image_link = quiz['image_link']
+        options = [str(option) for option in quiz['options']]
+        correct_answer = quiz['correct']
+        correct_id = 0
+
+        for i,option in enumerate(options):
+            if correct_answer == option:
+                correct_id = i
+
+        if i == 0:
+            query.edit_message_text(text=start)
+            bot.sendPhoto(chat_id,image_link,caption='#python #test #print')
+            bot.send_poll(chat_id, question,options,is_anonymous=False,type='quiz',correct_option_id=correct_id,open_period=5)
+        else:
+            bot.sendPhoto(chat_id,image_link,caption='#python #test #print')
+            bot.send_poll(chat_id, question,options,is_anonymous=False,type='quiz',correct_option_id=correct_id,open_period=5)
 
 
 updater = Updater('1514048549:AAEKmDPsPzDIw3K4CBIPxUqko2AzQ7gCyQA')
@@ -26,7 +51,7 @@ updater = Updater('1514048549:AAEKmDPsPzDIw3K4CBIPxUqko2AzQ7gCyQA')
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('quiz', quiz))
 updater.dispatcher.add_handler(MessageHandler(Filters.document,document))
-
+updater.dispatcher.add_handler(CallbackQueryHandler(choose_quiz))
 
 updater.start_polling()
 updater.idle()
